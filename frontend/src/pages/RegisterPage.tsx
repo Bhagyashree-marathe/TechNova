1d9204d5ed9bbd176c7061bd47d08cc1fc8a5cc8
Bhagyashree Marathe <bvmarathe07@gmail.com>
2026-08-30 22:52:38 +0530

    Update RegisterPage.tsx

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -25,7 +25,7 @@ function RegisterPage() {
   setError("")
   setIsLoading(true)
     try {
-    const response = await fetch("http://localhost:5000/api/auth/register", {
+    const response = await fetch("https://technova-1-h8od.onrender.com/api/auth/register", {, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
